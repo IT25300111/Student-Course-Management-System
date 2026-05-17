e5bea8b11499135a9fc340d05f789056413abb0e
IT25102514 <it25102514@my.sliit.lk>
2026-05-17 20:37:56 +0530

    Update admin.java

--- src/main/java/com/courseRegistration/studentRegistration/model/Admin.java
+++ src/main/java/com/courseRegistration/studentRegistration/model/Admin.java
@@ -1,69 +1,35 @@
 package com.courseRegistration.studentRegistration.model;
 
+import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "admins")
-public class Admin {
+public class Admin extends BaseUser {  // extends BaseUser
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private String password;
+    private String permissionLevel = "ADMIN";
 
     public Admin() {
+        super();
     }
 
     public Admin(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        super(name, email, password);  // Call parent constructor
     }
 
-    public Long getId() {
-        return id;
+
+    @Override
+    public String getRole() {
+        return "ADMIN";
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public String getDisplayInfo() {
+        return String.format("Admin: %s (%s)", getName(), permissionLevel);
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPermissionLevel() { return permissionLevel; }
+    public void setPermissionLevel(String permissionLevel) { this.permissionLevel = permissionLevel; }
 }
