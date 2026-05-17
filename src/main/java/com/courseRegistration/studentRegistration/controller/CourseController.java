8717593ae102c1e0facc245931bae639afd09acf
IT25300111 <IT25300111@my.sliit.lk>
2026-05-17 23:28:48 +0530

    Add CourseController with CRUD operations

--- src/main/java/com/courseRegistration/studentRegistration/controller/CourseController.java
+++ src/main/java/com/courseRegistration/studentRegistration/controller/CourseController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/courses")
-@CrossOrigin(origins = "*")
 public class CourseController {
     private final CourseService courseService;
 
